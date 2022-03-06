6649955221c8aea40b24589962da4cc90b72ceef
jonathanleivag <email@jonathanleivag.cl>
2022-03-06 00:53:35 -0300

    Saque el logo de navbar en dimensiones pequeñas y agrege efecto al seleccionar un pokémon

--- components/page/home/CardHomePageComponent.tsx
+++ components/page/home/CardHomePageComponent.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { IResultPokemon } from '../../../interface'
 
 export interface IPropsHomePage {
@@ -7,22 +7,28 @@ export interface IPropsHomePage {
 }
 
 export const CardHomePageComponent: FC<IPropsHomePage> = ({ pokemon }) => {
+  const [selectEffect, setSelectEffect] = useState<string>('')
+
+  const handleClick = () => setSelectEffect('animate-ping')
+
   return (
-    <div className='text-white w-full h-40 flex flex-col bg-slate-800 transform duration-500 ease-in-out hover:scale-105 rounded-lg cursor-pointer'>
-      <div className='w-full h-[80%] flex flex-row justify-center items-center'>
-        <Image
-          src={pokemon.image}
-          alt={`Imagen del pokémon ${pokemon.image}`}
-          width={128}
-          height={128}
-        />
-      </div>
-      <div className='w-full h-[20%] flex flex-row px-[5%]'>
-        <div className='w-1/2 h-full'> {pokemon.name} </div>
-        <div className='w-1/2 flex flex-row justify-end h-full'>
-          {`#${pokemon.id}`}
+    <button className={selectEffect} onClick={handleClick}>
+      <div className='text-white w-full h-40 flex flex-col bg-slate-800 transform duration-500 ease-in-out hover:scale-105 rounded-lg cursor-pointer'>
+        <div className='w-full h-[80%] flex flex-row justify-center items-center'>
+          <Image
+            src={pokemon.image}
+            alt={`Imagen del pokémon ${pokemon.image}`}
+            width={128}
+            height={128}
+          />
+        </div>
+        <div className='w-full h-[20%] flex flex-row px-[5%]'>
+          <div className='w-1/2 h-full'> {pokemon.name} </div>
+          <div className='w-1/2 flex flex-row justify-end h-full'>
+            {`#${pokemon.id}`}
+          </div>
         </div>
       </div>
-    </div>
+    </button>
   )
 }
--- components/ui/NavbarUiComponent.tsx
+++ components/ui/NavbarUiComponent.tsx
@@ -4,12 +4,14 @@ export const NavbarUiComponent = () => {
   return (
     <nav className='flex flex-row text-white px-3'>
       <div className='w-1/2 py-2 flex flex-row justify-start text-[#FDCB06] stroke-text items-center font-pokemon-solid text-center'>
-        <Image
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-          width={70}
-          height={70}
-          alt='Pokémon charmander'
-        />
+        <div className='hidden md:block'>
+          <Image
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
+            width={80}
+            height={80}
+            alt='Pokémon charmander'
+          />
+        </div>
         <p className='text-4xl'>P</p>
         <p className='text-2xl'>okémon Api</p>
       </div>
